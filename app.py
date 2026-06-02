af799fd791a666bde2267aefc6d9346469e0656b
hered78 <chhemanth78@gmail.com>
2026-06-02 10:51:58 +0530

    changes to app file

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
-print("Connecting to local Kubernetes cluster...")
+age=int(input('what is you age'))
+if age>=20:
+   print('learn how to work')
+else:
+    print("Connecting to local Kubernetes cluster...")
 # Future step: Add kubernetes client python library to check cluster nodes
