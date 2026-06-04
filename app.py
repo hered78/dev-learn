f68d76fc1addc721424a691cb877e0340473fbe7
hered78 <chhemanth78@gmail.com>
2026-06-04 22:25:47 +0530

    checking diff

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ if age>=20:
    print('learn how to work')
 else:
     print("Connecting to local Kubernetes cluster...")
+    print("po ra padhi")
 # Future step: Add kubernetes client python library to check cluster nodes
